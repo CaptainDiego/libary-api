3754240cd96df0f963cf49364f4737ee9b75a88e
CaptainDiego <diego.gilon@gmail.com>
2020-05-18 15:15:18 -0500

    Add a search method

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,3 +1,3 @@
 class LeadsController < ApplicationController
-  
+      
 end
--- app/resources/lead_resource.rb
+++ app/resources/lead_resource.rb
@@ -1,3 +1,15 @@
 class LeadResource < JSONAPI::Resource
   attributes :first, :last, :email
+  filters :query
+
+  def self.apply_filter(record, filter, value, options)
+    case filter
+      when :query
+        records.where('last LIKE ?', "%#{value.first}%")
+          .or(records.where('first LIKE ?', "%#{value.first}%"))
+          .or(records.where('email LIKE ?', "%#{value.first}%"))
+      else
+        super
+    end
+  end
 end

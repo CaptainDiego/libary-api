2d1ef12ea5a8f660ff290f389cf8017fc019324a
CaptainDiego <diego.gilon@gmail.com>
2020-05-27 21:41:41 -0500

    Adjust lead endpoints for new serializer

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,3 +1,29 @@
 class LeadsController < ApplicationController
-      
+  def index
+    render json: LeadSerializer.new(Lead.all).serialized_json
+  end
+
+  def create
+    lead = Lead.create(lead_params)
+    render json: LeadSerializer.new(lead).serialized_json
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    Lead.find(params['id']).destroy
+
+    head :no_content
+  end
+
+  private
+
+
+  def lead_params
+    params.require(:data)
+          .require(:attributes)
+          .permit(:first, :last, :email)
+  end
 end

bf8af212635360b496bd28b5133ad440b708fe94
CaptainDiego <diego.gilon@gmail.com>
2020-06-04 18:23:07 -0500

    Include authentication module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::API
-  def authenticate
-      authenticate_or_request_with_http_token do |token, _options|
-        verified_token = JWT.decode(
-          token, ENV['JWT_SECRET'],
-          true,
-          { algorithm: 'HS512' }
-        )
+  include ActionController::HttpAuthentication::Token::ControllerMethods
 
-        user_id = verified_token.first['user_id']
-        @current_user = User.find(user_id)
-      end
+  def authenticate
+    authenticate_or_request_with_http_token do |token, _options|
+      verified_token = JWT.decode(
+        token, ENV['JWT_SECRET'],
+        true,
+        { algorithm: 'HS512' }
+      )
+      user_id = verified_token.first['user_id']
+      @current_user = User.find(user_id)
+    end
     rescue
       render status: :unauthorized
   end

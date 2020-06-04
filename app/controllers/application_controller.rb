8508d286a10ce357388553918c9be7fd34655255
CaptainDiego <diego.gilon@gmail.com>
2020-06-04 18:30:37 -0500

    Refactor authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,12 @@ class ApplicationController < ActionController::API
   def authenticate
     authenticate_or_request_with_http_token do |token, _options|
       verified_token = JWT.decode(
-        token, ENV['JWT_SECRET'],
+        token, 
+        JWT_SECRET,
         true,
         { algorithm: 'HS512' }
       )
+      
       user_id = verified_token.first['user_id']
       @current_user = User.find(user_id)
     end
